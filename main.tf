19d06653b3e8660a1795d21e3c8bd283042de0d8
BecomeDevops <84931173+BecomeDevops@users.noreply.github.com>
2024-03-20 04:03:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,17 @@ resource "aws" {
 
     region = "us-west-1"
 }
+
+
+resource "aws_instance" "tf_ec2" {
+
+
+instance_type = "t2.micro"
+ami = "ami-0a0409af1cb831414"
+key_name = "elbtg"
+tags = {
+
+Name = "tf_ec2_gh"
+}
+
+}
